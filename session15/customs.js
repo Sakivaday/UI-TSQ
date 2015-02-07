7f07e00b84c803e758f67d95b840e1dc30020966
Sakivaday <vyadav@g.clemson.edu>
2015-02-07 13:40:12 -0600

    dashboard

---
+++ session15/customs.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	$('.show-all').click(function(event) {
+		event.preventDefault();
+		$.ajax({
+			url : 'data/data.json',
+			success : function(result) {
+				$.each(result, function(key, value) {
+					var tr = "<tr><td>" + (key + 1) + "</td><td>" + value.FirstName + "</td><td>" + value.LastName + "</td></tr>"
+					$('.contacts tbody').append(tr);
+				});
+			},
+			error : function() {
+				alert("error");
+			}
+		});
+	});
+	$('.search').click(function(event) {
+		event.preventDefault();
+		$.ajax({
+			url : 'data/data.json',
+			success : function(result) {
+				$.each(result, function(key, value) {
+					if ($('#account').val() == value.Account) {
+					var tr = "<tr><td>" + (key + 1) + "</td><td>" + value.FirstName + "</td><td>" + value.LastName + "</td></tr>"
+					$('.contacts tbody').append(tr);
+				} });
+			},
+			error : function() {
+				alert("error");
+			}
+		});
+	});
+
+});
+
