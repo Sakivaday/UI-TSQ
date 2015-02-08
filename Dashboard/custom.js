975c35f7a76cad872be79db02bb742d98aff387c
Sakivaday <vyadav@g.clemson.edu>
2015-02-08 13:49:18 -0600

    upd.

--- Dashboard/custom.js
+++ Dashboard/custom.js
@@ -1,3 +1,37 @@
-/**
- * @author vikas
- */
+$(document).ready(function() {
+	$(function() {
+
+		$("#loginpage").validate({
+			errorLabelContainer : '#errors'
+		});
+
+		$('.signin').click(function(e) {
+
+			if ($("#loginpage").valid()) {
+				alert('valid!');
+				return false;
+			} else {
+				alert('invalid!');
+				return false;
+			}
+		});
+
+	});
+	$('.signin').click(function(event) {
+		event.preventDefault();
+		$.ajax({
+			url : 'users.json',
+			success : function(result) {
+				$.each(result, function(key, value) {
+					if (($('#email').val() == value.Email) && ($('#password').val() == value.Password)) {
+ 						 window.location='searchbox.html';
+					}
+				});
+			},
+			error : function() {
+				alert("error");
+			}
+		});
+	});
+
+});
