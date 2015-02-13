9b6dfd7f0d412933012b748d36d49fd1c4b480e9
Sakivaday <vyadav@g.clemson.edu>
2015-02-12 18:07:08 -0600

    4

--- Dashboard/custom.js
+++ Dashboard/custom.js
@@ -16,21 +16,30 @@ $(document).ready(function() {
 	});
 	$('.signin').click(function(event) {
 		event.preventDefault();
+		var sign = false;
 		$.ajax({
-			url : 'users.json',
+			url : 'userinfo.json',
 			success : function(result) {
 				$.each(result, function(key, value) {
 					if (($('#email').val() == value.Email) && ($('#password').val() == value.Password)) {
-						window.location = 'searchbox.html';
+						sign = true;
+						return;
 					}
 
 				});
+				if (sign) {
+					window.location = 'searchbox.html';
+
+				} else {
+					$('#errors').text('User Not Found!!');
+				}
 			},
 			error : function() {
 				alert("error");
 			}
 		});
 	});
+
 	$('.searchbtn').click(function(event) {
 		event.preventDefault();
 		$.ajax({
@@ -41,12 +50,42 @@ $(document).ready(function() {
 						var tr = "<tr><td>" + value.FirstName + "</td><td>" + value.LastName + "</td><td>" + value.Account + "</td><td>" + value.Relationship + "</td></tr>"
 						$('.contact tbody').append(tr);
 					}
-					
+
 				});
 			},
 			error : function() {
 				alert("error");
 			}
+		});
+	});
+
+	$('ul.tabs').each(function() {
+
+		var $active,
+		    $content,
+		    $links = $(this).find('a');
+
+		$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+		$active.addClass('active');
+
+		$content = $($active[0].hash);
+
+		$links.not($active).each(function() {
+			$(this.hash).hide();
+		});
+
+		$(this).on('click', 'a', function(e) {
+
+			$active.removeClass('active');
+			$content.hide();
+
+			$active = $(this);
+			$content = $(this.hash);
+
+			$active.addClass('active');
+			$content.show();
+
+			e.preventDefault();
 		});
 	});
 
