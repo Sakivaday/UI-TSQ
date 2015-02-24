c429d1d02d5d5f8a33aa8535fd32956a710f4482
Sakivaday <vyadav@g.clemson.edu>
2015-02-23 23:12:26 -0600

    assgn

--- session15/customs.js
+++ session15/customs.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('.show-all').click(function(event) {
 		event.preventDefault();
 		$.ajax({
-			url : 'data/data.json',
+			url : 'updata/updata.json',
 			success : function(result) {
 				$.each(result, function(key, value) {
 					var tr = "<tr><td>" + (key + 1) + "</td><td>" + value.FirstName + "</td><td>" +
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	$('.search').click(function(event) {
 		event.preventDefault();
 		$.ajax({
-			url : 'data/data.json',
+			url : 'data/updata.json',
 			success : function(result) {
 				$.each(result, function(key, value) {
 					if ($('#account').val() == value.Account) {
